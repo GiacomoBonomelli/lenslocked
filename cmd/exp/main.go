e682ce1bd7c167e6d1efca4c5466bbc12f4f9049
Giacomo <gbonomelli483@gmail.com>
2025-04-22 14:53:04 +0200

    Added postgres-config struct

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -6,9 +6,28 @@ import (
 
 	_ "github.com/jackc/pgx/v4/stdlib"
 )
+type PostgresConfig struct{
+	Host string
+	Port string
+	User string
+	Password string
+	Database string
+	SSLMode string
+}
 
+func (cfg PostgresConfig) String() string{
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",cfg.Host,cfg.Port,cfg.User,cfg.Password,cfg.Database,cfg.SSLMode)
+}
 func main() {
-	db, err := sql.Open("pgx", "host=localhost port=5434 user=baloo password=junglebook dbname=lenslocked sslmode=disable")
+	cfg := PostgresConfig{
+		Host:"localhost",
+		Port:"5434",
+		User:"baloo",
+		Password:"junglebook",
+		Database:"lenslocked",
+		SSLMode:"disable",
+	}
+	db, err := sql.Open("pgx", cfg.String())
 	if err != nil {
 		panic(err)
 	}
