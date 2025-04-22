723fb4ad741a2c3770696ee70ddc08d12d717f40
Giacomo <gbonomelli483@gmail.com>
2025-04-22 17:28:51 +0200

    Added the request for the id of the user when creating a new user

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -58,11 +58,16 @@ func main() {
 	// Insert some data
 	name := "Giacomo"
 	email := "bonomellisrl@gmail.com"
-	_, err = db.Exec(`
+	/* _, err = db.Exec(`
 		INSERT INTO users (name,email) 
-		VALUES ($1,$2);`, name, email)
+		VALUES ($1,$2);`, name, email) */
+	row= db.QueryRow(`
+		INSERT INTO users (name,email) 
+		VALUES ($1,$2) RETURNING ID;`, name, email))
+	var id int
+	err = row.Scan(&id)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("User created")
+	fmt.Println("User created. id=",id)
 }
