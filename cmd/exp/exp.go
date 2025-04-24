a3dcb82a6e1bea8ec420cc1555b86c2f18ae199d
Giacomo <gbonomelli483@gmail.com>
2025-04-24 17:33:28 +0200

    Added a postgres config file

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -1,56 +1,32 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
-	_ "github.com/jackc/pgx/v4/stdlib"
+
 	"github.com/GiacomoBonomelli/lenslocked/models"
 )
 
-type PostgresConfig struct {
-	Host     string
-	Port     string
-	User     string
-	Password string
-	Database string
-	SSLMode  string
-}
-
-func (cfg PostgresConfig) String() string {
-	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.Database, cfg.SSLMode)
-}
 func main() {
-	cfg := PostgresConfig{
-		Host:     "localhost",
-		Port:     "5434",
-		User:     "baloo",
-		Password: "junglebook",
-		Database: "lenslocked",
-		SSLMode:  "disable",
-	}
-	db, err := sql.Open("pgx", cfg.String())
+
+	cfg := models.DefaultPostgresConfig()
+	db, err := models.Open(cfg)
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Connected!")
 
-	us:= models.UserService{
+	us := models.UserService{
 		DB: db,
 	}
 
-	user,err := us.Create("gbonomelli483@gmail.com","abc123")
-	
-	if err!=nil{
+	user, err := us.Create("jon@calhoun.com", "abc123")
+
+	if err != nil {
 		panic(err)
 	}
 	fmt.Println(user)
 
-	 /* _, err = db.Exec(`
+	/* _, err = db.Exec(`
 		CREATE TABLE IF NOT EXISTS users(
 			id SERIAL PRIMARY KEY,
 			name TEXT,
@@ -106,36 +82,36 @@ func main() {
 	}
 	fmt.Println("Created fake orders.") */
 	/*
-	userID := 1
-	type Order struct {
-		ID          int
-		UserID      int
-		Amount      int
-		Description string
-	}
-	var orders []Order
-	rows, err := db.Query(
-		`SELECT id,amount,description
-		FROM orders
-		WHERE user_id=$1;`, userID)
-	if err != nil {
-		panic(err)
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		var order Order
-		order.UserID = userID
-		err := rows.Scan(&order.ID, &order.Amount, &order.Description)
+		userID := 1
+		type Order struct {
+			ID          int
+			UserID      int
+			Amount      int
+			Description string
+		}
+		var orders []Order
+		rows, err := db.Query(
+			`SELECT id,amount,description
+			FROM orders
+			WHERE user_id=$1;`, userID)
 		if err != nil {
 			panic(err)
 		}
-		orders = append(orders, order)
-	}
-	err = rows.Err()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Orders:", orders) */
- 
+		defer rows.Close()
+
+		for rows.Next() {
+			var order Order
+			order.UserID = userID
+			err := rows.Scan(&order.ID, &order.Amount, &order.Description)
+			if err != nil {
+				panic(err)
+			}
+			orders = append(orders, order)
+		}
+		err = rows.Err()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Orders:", orders) */
+
 }
