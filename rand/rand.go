f7e6ddcae3612b4b0f1f360b58327df4ccb77677
Giacomo <gbonomelli483@gmail.com>
2025-05-05 16:01:40 +0200

    Added the rand package to create new session tokens

---
+++ rand/rand.go
@@ -0,0 +1,36 @@
+package rand
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+)
+
+func Bytes(n int) ([]byte, error) {
+	//creo una slice di byte
+	b := make([]byte, n)
+	nRead, err := rand.Read(b)
+	if err != nil {
+		return nil, fmt.Errorf("bytes:%w", err)
+	}
+	if nRead < n {
+		return nil, fmt.Errorf("bytes: didn't read enough random bytes")
+	}
+	return b, nil
+}
+
+// Quando dovrò creare dei token per le sessions, chiamo solamente questa funzione
+func String(n int) (string, error) {
+	//fa un encoding della slice restituita dalla funz. Bytes
+	b, err := Bytes(n)
+	if err != nil {
+		return "", fmt.Errorf("string:%w", err)
+	}
+	return base64.URLEncoding.EncodeToString(b), nil
+}
+
+const SessionTokenBytes = 32
+
+func SessionToken() (string, error) {
+	return String(SessionTokenBytes)
+}
