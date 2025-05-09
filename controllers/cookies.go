b243965488d8052fee60a54f49b82c3affa88ae5
Giacomo <gbonomelli483@gmail.com>
2025-05-09 15:45:37 +0200

    Added the delete handler to the users controller

--- controllers/cookies.go
+++ controllers/cookies.go
@@ -19,15 +19,21 @@ func newCookie(name, value string) *http.Cookie {
 	return &cookie
 }
 
-func setCookie(w http.ResponseWriter, name,value string){
-	cookie:= newCookie(name,value)
-	http.SetCookie(w,cookie)
+func setCookie(w http.ResponseWriter, name, value string) {
+	cookie := newCookie(name, value)
+	http.SetCookie(w, cookie)
 }
 
-func readCookie(r *http.Request, name string) (string,error){
+func readCookie(r *http.Request, name string) (string, error) {
 	c, err := r.Cookie(name)
-	if err!= nil {
-		return "",fmt.Errorf("%s:%w",name,err)
+	if err != nil {
+		return "", fmt.Errorf("%s:%w", name, err)
 	}
-	return c.Value,nil
+	return c.Value, nil
+}
+
+func deleteCookie(w http.ResponseWriter, name string) {
+	cookie := newCookie(name, "")
+	cookie.MaxAge = -1
+	http.SetCookie(w, cookie)
 }
